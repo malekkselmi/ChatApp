8a1e57cf99b1975bfb78abac78144cabf3afbc3d
malekkselmi <61968749+malekkselmi@users.noreply.github.com>
2023-06-25 16:00:57 +0100

    Update Welcome.js

--- frontend/src/components/Welcome.js
+++ frontend/src/components/Welcome.js
@@ -1,20 +1,23 @@
 import React, { useState, useEffect } from "react";
 import styled from "styled-components";
-import Robot from "../assets/robot.gif";
+
 export default function Welcome() {
   const [userName, setUserName] = useState("");
-  useEffect(async () => {
-    setUserName(
-      await JSON.parse(
-        localStorage.getItem(process.env.REACT_APP_LOCALHOST_KEY)
-      ).username
-    );
+
+  useEffect( () => {
+    const setLocalstorage = async() => {
+        setUserName(
+            await JSON.parse(
+              localStorage.getItem(process.env.REACT_APP_LOCALHOST_KEY)
+            ).username
+          );
+    }
+       setLocalstorage()
   }, []);
   return (
     <Container>
-      <img src={Robot} alt="" />
       <h1>
-        Welcome, <span>{userName}!</span>
+        Welcome <span>{userName} !</span>
       </h1>
       <h3>Please select a chat to Start messaging.</h3>
     </Container>
@@ -27,9 +30,6 @@ const Container = styled.div`
   align-items: center;
   color: white;
   flex-direction: column;
-  img {
-    height: 20rem;
-  }
   span {
     color: #4e0eff;
   }
